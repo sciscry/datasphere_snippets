41c0398da0d5406aae900bc48686ff659ce1f4d9
Philipp Beer <philipp@Philipps-MacBook-Pro.local>
2025-03-04 16:47:46 +0100

    convert to timestamp

---
+++ convert_to_timestamp.sql
@@ -0,0 +1,22 @@
+-- replace FieldDateTime with field to be checked
+-- verify else-branch if error value matches expectation
+-- 100-year and 400-year rule in leap year rules are ignored
+
+CASE
+    WHEN SUBSTRING(TO_VARCHAR(FieldDateTime), 5, 2) BETWEEN '01' AND '12'
+    AND SUBSTRING(TO_VARCHAR(FieldDateTime), 7, 2) BETWEEN '01' AND
+        CASE
+            WHEN SUBSTRING(TO_VARCHAR(FieldDateTime), 5, 2) IN ('01', '03', '05', '07', '08', '10', '12') THEN '31'
+            WHEN SUBSTRING(TO_VARCHAR(FieldDateTime), 5, 2) IN ('04', '06', '09', '11') THEN '30'
+            WHEN SUBSTRING(TO_VARCHAR(FieldDateTime), 5, 2) = '02' THEN
+                CASE
+                    WHEN MOD(TO_INT(SUBSTRING(TO_VARCHAR(FieldDateTime), 1, 4)), 4) = 0 THEN '29'
+                    ELSE '28'
+                END
+        END
+    AND SUBSTRING(TO_VARCHAR(FieldDateTime), 9, 2) BETWEEN '00' AND '23'
+    AND SUBSTRING(TO_VARCHAR(FieldDateTime), 11, 2) BETWEEN '00' AND '59'
+    AND SUBSTRING(TO_VARCHAR(FieldDateTime), 13, 2) BETWEEN '00' AND '59'
+    THEN TO_TIMESTAMP(TO_VARCHAR(FieldDateTime), 'YYYYMMDDHHMMSS')
+    ELSE TO_TIMESTAMP('19700101000000', 'YYYYMMDDHHMMSS')
+END
